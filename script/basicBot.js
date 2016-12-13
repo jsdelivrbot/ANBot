71b6d806d393e9e0d193ad7b32212f804f9db21e
SkillsAura <6randomemail9@gmail.com>
2016-12-12 19:33:40 -0500

    Update basicBot.js

--- script/basicBot.js
+++ script/basicBot.js
@@ -1705,8 +1705,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        API.sendChat("\/me »AN» We suggest RCS!");
-						API.sendChat("\/me https://rcs.radiant.dj/");
+                        API.sendChat("\/me »AN» We suggest RCS! https://rcs.radiant.dj/");
                     }
                 }
             },
