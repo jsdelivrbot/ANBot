a52500fe8e76e1127fc982298bede211fe43c19a
SkillsAura <6randomemail9@gmail.com>
2016-12-14 20:23:47 -0500

    Update basicBot.js

--- script/basicBot.js
+++ script/basicBot.js
@@ -3622,7 +3622,20 @@
 							API.sendChat("\/me »AN» Harambe, you will always be remembered!!");	
                     }
                 }
-            },							
+            },	
+            datboiCommand: {
+                command: 'datboi',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        if (typeof basicBot.settings.website === "string")		
+							API.sendChat("\/me »AN» oshit, waddup!");	
+                    }
+                }
+            },				
             whoisCommand: {
                 command: 'whois',
                 rank: 'bouncer',
