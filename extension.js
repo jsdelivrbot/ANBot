d9e138c3fa453fc8148c4ee06e90e2bb213d6769
MrRoboto-TFSNetwork <admin@tfs-network.com>
2017-02-12 17:54:14 -0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -84,7 +84,7 @@
             if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
             if (!bot.commands.executable(this.rank, chat)) return void (0);
             else {
-              var staff = API.getStaff();
+              var staff = API.getStaff().username;
               API.sendChat("/me Current staffs online: " + staff);
             }
           }
